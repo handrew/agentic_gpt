019a7ad27e7bffc8424760df4fc45d9dd44a811f
Andrew Han <handrew11@gmail.com>
2023-06-04 18:06:51 -0700

    decompose indexing helper functions

--- agentic_gpt/agent/memory.py
+++ agentic_gpt/agent/memory.py
@@ -5,41 +5,8 @@ takes care of embedding the documents, indexing them, and retrieving
 them, as well as the bookkeeping of the documents.
 """
 from llama_index import Document
-from llama_index import GPTVectorStoreIndex, GPTListIndex
-from llama_index import ServiceContext, LLMPredictor
-from langchain.chat_models import ChatOpenAI
-from typing import Dict
-from tqdm import tqdm
-
-CHATGPT_KWARGS = {"temperature": 0, "model_name": "gpt-3.5-turbo"}
-
-
-def init_index(docs, index_type="vector"):
-    """Initialize each index with a different service context."""
-    assert index_type in ("vector", "list")
-    llm = LLMPredictor(llm=ChatOpenAI(**CHATGPT_KWARGS))
-    service_context = ServiceContext.from_defaults(llm_predictor=llm)
-    if index_type == "vector":
-        index = GPTVectorStoreIndex.from_documents(
-            docs, service_context=service_context
-        )
-    elif index_type == "list":
-        index = GPTListIndex.from_documents(docs, service_context=service_context)
-
-    return index
-
-
-def summarize_documents(docs):
-    """Given a list of documents, create a list of their summaries."""
-    summaries = []
-    indexes = []
-    for doc in docs:
-        index = init_index([doc], index_type="vector")
-        query_engine = index.as_query_engine(response_mode="tree_summarize")
-        response = query_engine.query("Summarize in a few sentences: ")
-        summaries.append(response.response)
-        indexes.append(index)
-    return {"summaries": summaries, "indexes": indexes}
+from typing import DictDocument()
+from .utils.indexing import init_index, summarize_documents
 
 
 class Memory:
